cbeeb27a7fcca019fee2893ad4b37885423083ce
Matt Lehrer <lehrerm@gmail.com>
2022-02-19 22:47:23 +0100

    fix: get ethers to run in a cf worker

--- build.js
+++ build.js
@@ -1,20 +1,31 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { build } from "esbuild";
+import { build } from 'esbuild';
+import { replace } from 'esbuild-plugin-replace';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 try {
-  await build({
-    bundle: true,
-    sourcemap: true,
-    format: "esm",
-    target: "esnext",
-    entryPoints: [path.join(__dirname, "src", "index.ts")],
-    outdir: path.join(__dirname, "dist"),
-    outExtension: { ".js": ".mjs" },
-  });
+	await build({
+		bundle: true,
+		sourcemap: true,
+		format: 'esm',
+		target: 'esnext',
+		entryPoints: [path.join(__dirname, 'src', 'index.ts')],
+		outdir: path.join(__dirname, 'dist'),
+		outExtension: { '.js': '.mjs' },
+		plugins: [
+			replace({
+				values: {
+					[`request.mode`]: '//request.mode',
+					'request.cache': '//request.cache',
+					'request.credentials': '//request.credentials',
+					'request.referrer': '//request.referrer',
+				},
+			}),
+		],
+	});
 } catch {
-  process.exitCode = 1;
+	process.exitCode = 1;
 }
