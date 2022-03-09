a133549ec9ad773db8aa20d70834a8d24941d504
Matt Lehrer <lehrerm@gmail.com>
2022-03-09 23:47:11 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { ethers } from 'ethers';
+import { abi } from './abi';
 
+const CORRUPTIONS_CONTRACT_ADDR = '0x5BDf397bB2912859Dbd8011F320a222f79A28d2E';
+// const CORRUPTIONS_DEPLOYMENT_BLOCK = 13604427; // actual
+const CORRUPTIONS_DEPLOYMENT_BLOCK = 13605000; // rounded and after all mints
 const numBlocksToSkip = 6500;
+const ALCHEMY_FETCHES_IN_PARALLEL = 100;
 
 export async function handleRequest(request: Request, env: Bindings) {
 	const { CORRUPTIONS, ALCHEMY_URL } = env;
@@ -9,14 +14,24 @@ export async function handleRequest(request: Request, env: Bindings) {
 		if (typeof json !== 'object' || json === null) {
 			return new Response('Invalid JSON', { status: 400 });
 		}
+		const { tokenid, count } = json;
 		const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_URL);
 		const latestBlock = await provider.getBlockNumber();
 		// console.log(latestBlock);
 		// const latestBlock =
 		// 	Math.floor((await provider.getBlockNumber()) / numBlocksToSkip) *
 		// 	numBlocksToSkip;
+		const corruptionsContract = new ethers.Contract(
+			CORRUPTIONS_CONTRACT_ADDR,
+			abi.abi,
+			provider,
+		);
+		const res: string = await corruptionsContract.tokenURI(tokenid, {
+			// https://docs.ethers.io/v5/api/providers/types/#providers-BlockTag
+			blockTag: latestBlock,
+		});
 
-		return new Response('hey');
+		return new Response(res);
 	} catch (error) {
 		console.log(JSON.stringify(error, null, 2));
 		return new Response(JSON.stringify(error, null, 2), { status: 400 });
